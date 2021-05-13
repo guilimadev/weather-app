0af765d2a2efb5d9848caed9cbde32a002db00e1
Guilherme <guilimadev@gmail.com>
2021-05-13 18:30:15 -0300

    Only left to resolve broken GMT times

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeathter] = useState({});
   const [dayornight, setDayorNight] = useState('');
+  const [otherDay, setOtherDay] = useState(false)
   const urlIcon = 'http://openweathermap.org/img/wn/';
 
 
@@ -28,30 +29,78 @@ function App() {
         
       if(typeof result.main != "undefined"){
 
-
-        let sunset = result.sys.sunset
-        let date = new Date(sunset * 1000);      
-
-        
+        //Defining if it's day or night
+        let date = result.sys.sunset
+        let sunset = new Date(date * 1000);   
+  
+                
         let now = new Date();      
-
-        let timezoneHours = now.getUTCHours() + result.timezone/3600;
-
-        console.log(date.getHours(), timezoneHours)
-        if(date.getHours() > timezoneHours){          
-          setDayorNight('Day');
+  
+        let timezoneHours = now.getUTCHours() + (result.timezone/3600);
+  
+        let x = new Date();
+        let localOffSet = x.getTimezoneOffset()
+        let diffOffset = (result.timezone/3600) + (localOffSet/60)
+  
+        let sunsethours = sunset.getHours() + diffOffset
+        if(sunsethours < 13){
+          sunsethours = 19;
+        }        
         
+        if(sunsethours < timezoneHours){
           
-        } else{
-      
-          setDayorNight('Night');
-      
-        }
-          console.log(dayornight)
-          console.log(result.sys.sunrise)
-      } 
-      });
+          setDayorNight('Night');                                    
+        } else if((sunsethours === timezoneHours) && (sunset.getMinutes() < now.getUTCMinutes())){
+          setDayorNight('Night');                 
+         } else {
+          setDayorNight('Day');         
+        } 
 
+        // Defining if already pass a day from UTC
+
+        let now2 = new Date();   
+   
+
+        let timezoneHours2 = now2.getUTCHours() + result.timezone/3600;
+      
+        
+        let minutes2 = `${now2.getUTCMinutes()}`
+        
+        if(now2.getUTCMinutes() < 10){
+          minutes2 = '0' + now2.getUTCMinutes()
+        }
+
+        
+        
+        if(timezoneHours2 >= 24){
+          setOtherDay(true);
+          timezoneHours = timezoneHours2 - 24         
+          if(timezoneHours2 < 10){
+            timezoneHours2 = '0' + timezoneHours2;
+          } 
+
+          let sunrisehours = result.sys.sunrise
+          
+          let sunrise = new Date(sunrisehours * 1000)
+          
+          if( (sunrise.getHours() + diffOffset) - 24 <= timezoneHours){
+            if(sunrise.getUTCMinutes() < now2.getUTCMinutes()){
+              setDayorNight('Day');
+            }
+            
+          }         
+        }
+        else {
+          setOtherDay(false)
+        }
+        
+
+               
+      } 
+
+     
+      });
+      dateBuilder(new Date()); 
       nightOrDay();
 
     }
@@ -60,24 +109,39 @@ function App() {
 
   const nightOrDay = () =>{
     if(typeof weather.main != "undefined") {  
-      let sunset = weather.sys.sunset
-      let date = new Date(sunset * 1000);      
+      let date = weather.sys.sunset
+      let sunset = new Date(date * 1000);   
+
+
+     
+
+      
 
       
       let now = new Date();      
 
-      let timezoneHours = now.getUTCHours() + weather.timezone/3600;
+      let timezoneHours = now.getUTCHours() + (weather.timezone/3600);
 
-      
-      if(date.getHours() > timezoneHours){          
-        setDayorNight('Day');
-       
-        
-      } else{
-     
-        setDayorNight('Night');
-    
+      let x = new Date();
+      let localOffSet = x.getTimezoneOffset()
+      let diffOffset = (weather.timezone/3600) + (localOffSet/60)
+
+      let sunsethours = sunset.getHours() + diffOffset
+      if(sunsethours < 13){
+        sunsethours = 19;
       }
+      
+      
+      if(sunsethours < timezoneHours){
+        setDayorNight('Night');
+                                  
+      } else if(sunsethours === timezoneHours && sunset.getMinutes > now.getUTCMinutes())       
+       {
+          setDayorNight('Night');                 
+       } else {
+       setDayorNight('Day');   
+    
+      } 
     }
   }
 
@@ -85,47 +149,51 @@ function App() {
     let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
     let days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
 
+    
     let day = days[d.getDay()];
     let date = d.getDate();
     
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-       
+    
+    if(otherDay){
+      day = days[d.getDay() + 1]
+      date = date + 1      
 
-    return `${day} ${date} ${month} ${year}`
+      return `${day} ${date} ${month} ${year}`      
+    }  else {
+      return `${day} ${date} ${month} ${year}`
+    }
+
+    
 
     
   }
 
-  const currentTime = (t) => {
-    //Sunset from current search result  
-    
-
+  const currentTime = () => {
+    //Sunset from current search result    
     
     let now = new Date();   
-   
-
+    
+    
     let timezoneHours = now.getUTCHours() + weather.timezone/3600;
+   
+    
+    let minutes = now.getUTCMinutes()    
+    
 
-
-    
-    
-    let minutes = `${now.getUTCMinutes()}`
-    
     if(now.getUTCMinutes() < 10){
       minutes = '0' + now.getUTCMinutes()
     }
-
-    if(timezoneHours >= 24){
+    
+    if(timezoneHours >= 24){     
       timezoneHours = timezoneHours - 24
-
-
 
       if(timezoneHours < 10){
         timezoneHours = '0' + timezoneHours;
       } 
     }
-
+    
     return `${timezoneHours}:${minutes}`
   }
 
@@ -135,10 +203,10 @@ function App() {
     
     <div className={
       (typeof weather.main != "undefined") ? 
-        (weather.main.temp < 17 && dayornight === 'Night') ? 'app coldnight' :     
-        (weather.main.temp < 17 && dayornight === 'Day') ? 'app coldday' :
-        (weather.main.temp > 17 && dayornight === 'Night') ? 'app warmnight' :
-        (weather.main.temp > 17 && dayornight === 'Day') ? 'app warmday' : 'app'
+        (weather.main.temp < 15 && dayornight === 'Night') ? 'app coldnight' :     
+        (weather.main.temp < 15 && dayornight === 'Day') ? 'app coldday' :
+        (weather.main.temp > 15 && dayornight === 'Night') ? 'app warmnight' :
+        (weather.main.temp > 15 && dayornight === 'Day') ? 'app warmday' : 'app'
       : 'app'}>
       <main>
         <div className="search-box">
@@ -158,7 +226,7 @@ function App() {
           <div>
             <div className="location-box">
               <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())} <br /> {dayornight} <br /> {currentTime()}</div>
+              <div className="date">{dateBuilder(new Date(), weather.sys.sunset)} <br /> {dayornight} <br /> Local time: {currentTime()}</div>
             </div>
 
             <div className="weather-box">
