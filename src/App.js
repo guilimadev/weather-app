23ff38121e2a51373ccb55473d0c511f4abb2769
Guilherme <guilimadev@gmail.com>
2021-05-13 10:39:14 -0300

    Background implementation

--- src/App.js
+++ src/App.js
@@ -6,21 +6,79 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeathter] = useState({});
+  const [dayornight, setDayorNight] = useState('');
+  const urlIcon = 'http://openweathermap.org/img/wn/';
+
+
 
   const search = evt => {
-    if(evt.key === "Enter") { 
+    
+    if(evt.key === "Enter") {      
+      
+
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then(res => res.json())
       .then(result => {
         setWeathter(result);
         setQuery('');
+        
+      if(typeof result.main != "undefined"){
+
+
+        let sunset = result.sys.sunset
+        let date = new Date(sunset * 1000);      
+
+        
+        let now = new Date();      
+
+        let timezoneHours = now.getUTCHours() + result.timezone/3600;
+
+        console.log(date.getHours(), timezoneHours)
+        if(date.getHours() > timezoneHours){          
+          setDayorNight('Day');
+        
+          
+        } else{
+      
+          setDayorNight('Night');
+      
+        }
+          console.log(dayornight)
+      } 
       });
+
+      nightOrDay();
+
     }
+        
   }
 
+  const nightOrDay = () =>{
+    if(typeof weather.main != "undefined") {  
+      let sunset = weather.sys.sunset
+      let date = new Date(sunset * 1000);      
+
+      
+      let now = new Date();      
+
+      let timezoneHours = now.getUTCHours() + weather.timezone/3600;
+
+      
+      if(date.getHours() > timezoneHours){          
+        setDayorNight('Day');
+       
+        
+      } else{
+     
+        setDayorNight('Night');
+    
+      }
+    }
+  }
 
   const dateBuilder = (d) => {
     let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
@@ -31,20 +89,46 @@ function App() {
     
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-
-   console.log({weather})
+       
 
     return `${day} ${date} ${month} ${year}`
 
     
   }
 
+  const currentTime = (t) => {
+    //Sunset from current search result  
+    
+
+    
+    let now = new Date();   
+   
+
+    let timezoneHours = now.getUTCHours() + weather.timezone/3600;
+
+
+    
+    
+    let minutes = now.getUTCMinutes();
+    
+    if(now.getUTCMinutes() < 10){
+      minutes = `0${now.getUTCMinutes}`
+    }
+
+    return `${timezoneHours}:${minutes}`
+  }
+
+  
+
   return (
     
-    <div className={(typeof weather.main != "undefined") 
-    ? ((weather.main.temp > 16) 
-    ? 'app warm' 
-    : 'app'): 'app'}>
+    <div className={
+      (typeof weather.main != "undefined") ? 
+        (weather.main.temp < 17 && dayornight === 'Night') ? 'app coldnight' :     
+        (weather.main.temp < 17 && dayornight === 'Day') ? 'app coldday' :
+        (weather.main.temp > 17 && dayornight === 'Night') ? 'app warmnight' :
+        (weather.main.temp > 17 && dayornight === 'Day') ? 'app warmday' : 'app'
+      : 'app'}>
       <main>
         <div className="search-box">
 
@@ -63,15 +147,13 @@ function App() {
           <div>
             <div className="location-box">
               <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
+              <div className="date">{dateBuilder(new Date())} <br /> {dayornight} <br /> {currentTime()}</div>
             </div>
 
             <div className="weather-box">
               <div className="temp"> {Math.round(weather.main.temp)}°c</div> 
               <div className="weather">{weather.weather[0].main}
-              <img className={(typeof weather.main != "undefined") 
-              ?  ((weather.weather[0].description === "scattered clouds") ? 'icon clouds' : (weather.weather[0].description === "clear sky") ? 'icon clear' : 'icon'): 'icon'}
-               src="" alt="" /></div>
+              <img src={`${urlIcon}${weather.weather[0].icon}@2x.png`} alt="" /></div>
             </div>
           </div>
         ): ('')}
